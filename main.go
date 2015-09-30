9a059b76f4f2c1b681121a627a711db71c01e33f
Jasper <Jasper_stein@hotmail.com>
2015-09-30 21:35:14 +0200

    Redo

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 import "fmt"
+import "os"
 func main() {
-    fmt.Println("Dockertest")
+    fmt.Println("Dockertest 2");
+    fmt.Println(os.Getenv("FOO")  +  "  ><<" + "thhat was foo");
+    fmt.Println(os.Getenv("Beer")  +  "  ><<" + "thhat was beer");
 }
