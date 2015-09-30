67970778286a90833a6fea4f5ed1483bc2523465
Jasper <Jasper_stein@hotmail.com>
2015-09-30 20:38:38 +0200

    wrong file test. Edited

--- lib/structs/stats/hello.go
+++ lib/structs/stats/hello.go
@@ -1,5 +1,7 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Dockertest")
+    fmt.Println("Dockertest 2");
+    fmt.Println(os.Getenv("FOO")  +  "  ><<" + "thhat was foo");
+    fmt.Println(os.Getenv("Beer")  +  "  ><<" + "thhat was beer");
 }
